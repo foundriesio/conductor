179fe1e7fc6cb789a7b5e68de98355b39658b0d3
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2021-08-24 12:32:29 +0100

    frontend: add footer with version
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- conductor/frontend/views.py
+++ conductor/frontend/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from conductor.core.models import Project
+from conductor.version import __version__ as app_version
 
 def index(request):
     projects = Project.objects.all()
-    context = {"projects": projects}
+    context = {"projects": projects, "version": app_version}
     return render(request, "conductor/home.html", context)
 
 
 def project(request, project_id):
     project = get_object_or_404(Project, pk=project_id)
-    context = {"project": project}
-    return render(request, "conductor/project.html", context) 
+    context = {"project": project, "version": app_version}
+    return render(request, "conductor/project.html", context)
