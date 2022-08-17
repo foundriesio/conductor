0fcab8aafe0c076f2fc4d1bff7c0075ac37187a4
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2022-08-17 10:13:35 +0100

    core: schedule u-boot upgrade test on imx8mm-sec
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- conductor/core/tasks.py
+++ conductor/core/tasks.py
@@ -294,7 +294,7 @@ def create_build_run(self, build_id, run_name):
                  "build": previous_build},
             ]
             # only imx8mm and imx6ull support u-boot OTA update
-            if run_name in ["imx8mmevk", "imx8mm-lpddr4-evk", "imx6ullevk", "imx8mp-lpddr4-evk"]:
+            if run_name in ["imx8mmevk", "imx8mm-lpddr4-evk", "imx6ullevk", "imx8mp-lpddr4-evk", "imx8mmevk-sec", "imx8mm-lpddr4-evk-sec"]:
                 templates.append(
                     {"name": "lava_uboot_rollback_template.yaml",
                      "job_type": LAVAJob.JOB_LAVA,
