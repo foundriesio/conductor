9e7b94c2efb14359b55a85d4a9a0bd27c2e7ca94
Milosz Wasilewski <milosz.wasilewski@foundries.io>
2022-11-09 09:30:34 +0000

    api: only trigger testing round on the proper branch
    When builds in a factory come from branch different than in Project
    settings, just ignore them. Only create testing round (additional build
    and test round) for the specified branch.
    
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@foundries.io>

--- conductor/api/views.py
+++ conductor/api/views.py
@@ -146,7 +146,8 @@ def process_jobserv_webhook(request):
         # build won't have a commit ID in conductor DB
         tag_build_runs.delay(build.pk)
         return HttpResponse("Created", status=201)
-    if "platform" in trigger_name:
+    # only trigger testing round when build comes from proper branch
+    if "platform" in trigger_name and project.default_branch in trigger_name:
         run_url = None
         build_run_list = []
         for run in request_body_json.get("runs"):
